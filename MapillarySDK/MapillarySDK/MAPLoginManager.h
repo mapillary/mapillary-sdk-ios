d8513439070bf141c390b040e95a507e390a3a18
Anders Mårtensson <anders@mapillary.com>
2017-11-01 12:39:13 +0100

    fix: added documentation

--- MapillarySDK/MapillarySDK/MAPLoginManager.h
+++ MapillarySDK/MapillarySDK/MAPLoginManager.h
@@ -9,12 +9,53 @@
 #import <Foundation/Foundation.h>
 #import "MAPUser.h"
 
+/** The `MAPLoginManager` class manages the authentication of Mapillary. The
+ * signIn method starts the authentication loop. It requires two values to be
+ * defined in your application `.plist` file:
+ *
+ * - MapillaryClientId
+ * - MapillaryRedirectUrl
+ *
+ * You need to register your app at mapillary.com to enter the redirect URL and
+ * also to obtain a client ID. Your app must also support the custom URL scheme
+ * defined in `MapillaryRedirectUrl`.
+ *
+ * @see MAPApplicationDelegate
+ */
 @interface MAPLoginManager : NSObject
 
+///-----------------------------------------------------------------------------
+/// @name Authentication
+///-----------------------------------------------------------------------------
+
+/** Redirects the user to an authentication page on mapillary.com in Safari.
+ * After the user has entered the credentials and is authenticated, focus is
+ * returned to the app.
+ *
+ * @param result A block object that is executed when the authentication loop
+ * finishes. This block has no return value and takes one argument: if
+ * authentication was sucessful or not.
+ */
 + (void)signIn:(void (^) (BOOL success))result;
+
+/** Signs out the current signed in user and removes all related user data. */
 + (void)signOut;
+
+///-----------------------------------------------------------------------------
+/// @name Current user
+///-----------------------------------------------------------------------------
+
+/** Checks if a user is signed in or not.
+ *
+ * @return YES if a user is signed in, NO if no user is signed in.
+ */
 + (BOOL)isSignedIn;
 
+/** Returns the currently signed in user.
+ *
+ * @return The signed in user if a user is signed in, nill if no user is signed
+ * in.
+ */
 + (MAPUser*)currentUser;
 
 @end
--- MapillarySDK/MapillarySDK/MAPUser.h
+++ MapillarySDK/MapillarySDK/MAPUser.h
@@ -8,11 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+/** The `MAPUser` class encapsulates the information about a user. */
 @interface MAPUser : NSObject
 
+/** The user's username. It can be changed by the user */
 @property (readonly) NSString* userName;
+
+/** The unique key to identify the user. The userKey never changes. */
 @property (readonly) NSString* userKey;
 
+/** Creates a user object.
+ *
+ * @param userName The user's user name.
+ * @param userKey The unique key to identify the user.
+ */
 - (id)initWithUserName:(NSString*)userName andUserKey:(NSString*)userKey;
 
 @end
