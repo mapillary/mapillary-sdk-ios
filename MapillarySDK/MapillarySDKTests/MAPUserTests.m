2e759814413c95689ebe76c4db6b457ba04f973f
Anders Mårtensson <anders@mapillary.com>
2018-03-06 10:01:07 +0100

    fix: wrote tests for MAPUser

--- MapillarySDK/MapillarySDKTests/MAPUserTests.m
+++ MapillarySDK/MapillarySDKTests/MAPUserTests.m
@@ -29,13 +29,31 @@
 {
     NSString* userName = @"Mappy";
     NSString* userKey = [[NSUUID UUID] UUIDString];
+    
+    MAPUser* user = [[MAPUser alloc] initWithUserName:userName andUserKey:userKey];
+    
+    XCTAssertTrue([userName isEqualToString:[user userName]]);
+    XCTAssertTrue([userKey isEqualToString:[user userKey]]);
+}
+
+- (void)testUserNameAndUserKeyAndAccessToken
+{
+    NSString* userName = @"Mappy";
+    NSString* userKey = [[NSUUID UUID] UUIDString];
     NSString* accessToken = [[NSUUID UUID] UUIDString];
     
-    MAPUser* user = [[MAPUser alloc] initWithUserName:userName andUserKey:userKey andUserAccessToken:accessToken];
+    MAPUser* user = [[MAPUser alloc] initWithUserName:userName andUserKey:userKey andAccessToken:accessToken];
     
-    XCTAssertTrue([userName isEqualToString:user.userName]);
-    XCTAssertTrue([userKey isEqualToString:user.userKey]);
-    XCTAssertTrue([accessToken isEqualToString:user.accessToken]);
+    XCTAssertTrue([userName isEqualToString:[user userName]]);
+    XCTAssertTrue([userKey isEqualToString:[user userKey]]);
+    XCTAssertTrue([accessToken isEqualToString:[user accessToken]]);
+}
+
+- (void)testNil
+{
+    MAPUser* user = [[MAPUser alloc] initWithUserName:nil andUserKey:nil andAccessToken:nil];
+    
+    XCTAssertNotNil(user);
 }
 
 @end
